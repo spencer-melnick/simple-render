6d87338fd27f66c2ca78ebcc0d9fb3fe3b323a41
Spencer Melnick <smelnick97@gmail.com>
2020-05-12 21:05:41 -0400

    Hooking in Vulkan debug messenger, using Vulkan-Hpp unique handles, and adding Vulkan-Hpp dynamic loader

--- src/rendering/instance.cpp
+++ src/rendering/instance.cpp
@@ -11,14 +11,25 @@
 #include <SDL2/SDL_vulkan.h>
 #include <vulkan/vulkan.hpp>
 
+
+// Allow default Vulkan-Hpp loader
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
+
+// Declaration for debug callback
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData);
+
+
 namespace Rendering
 {
     Instance::Instance()
     {
-        m_vulkanInstance = new vk::Instance(nullptr);
-
         initializeSdl();
         initializeVulkan();
+        setupVulkanDebug();
 
         auto extensionProperies = vk::enumerateInstanceExtensionProperties();
         spdlog::info("{} Vulkan extensions supported", extensionProperies.size());
@@ -26,15 +37,11 @@ namespace Rendering
 
     Instance::~Instance()
     {
-        spdlog::info("Destroying Vulkan instance");
-        m_vulkanInstance->destroy();
-        delete m_vulkanInstance;
-
         spdlog::info("Quitting SDL");
         SDL_Quit();
     }
 
-    vk::Instance& Instance::getVulkanInstance() const
+    const vk::Instance& Instance::getVulkanInstance() const
     {
         return *m_vulkanInstance;
     }
@@ -58,6 +65,12 @@ namespace Rendering
 
     void Instance::initializeVulkan()
     {
+        // Set up Vulkan dynamic loader to get access to extension functions
+        // SDL provides a function to get the vkGetInstanceProcAddress function pointer via its own chosen
+        // Vulkan loader
+        auto getInstanceProcAddr = static_cast<PFN_vkGetInstanceProcAddr>(SDL_Vulkan_GetVkGetInstanceProcAddr());
+        VULKAN_HPP_DEFAULT_DISPATCHER.init(getInstanceProcAddr);
+
         // Basic application info for driver support
         vk::ApplicationInfo applicationInfo;
         applicationInfo.pApplicationName = "Simple-Render";
@@ -67,10 +80,7 @@ namespace Rendering
         applicationInfo.apiVersion = VK_API_VERSION_1_2;
 
         // Get required extensions
-        unsigned int requiredExtensionCount;
-        SDL_Vulkan_GetInstanceExtensions(nullptr, &requiredExtensionCount, nullptr);
-        const char** requiredExtensionNames = new const char*[requiredExtensionCount];
-        SDL_Vulkan_GetInstanceExtensions(nullptr, &requiredExtensionCount, requiredExtensionNames);
+        auto extensions = getRequiredExtensions();
 
         // Query supported validation layers
         auto validationLayers = getSupportedValidationLayers();
@@ -78,15 +88,15 @@ namespace Rendering
         // Instance creation info
         vk::InstanceCreateInfo instanceInfo;
         instanceInfo.pApplicationInfo = &applicationInfo;
-        instanceInfo.enabledExtensionCount = requiredExtensionCount;
-        instanceInfo.ppEnabledExtensionNames = requiredExtensionNames;
+        instanceInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        instanceInfo.ppEnabledExtensionNames = extensions.data();
         instanceInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
         instanceInfo.ppEnabledLayerNames = validationLayers.data();
 
         spdlog::info("Creating Vulkan instance");
         try
         {
-            *m_vulkanInstance = vk::createInstance(instanceInfo);
+            m_vulkanInstance = vk::createInstanceUnique(instanceInfo);
         }
         catch (const std::exception& exception)
         {
@@ -94,35 +104,121 @@ namespace Rendering
             spdlog::error("Failed to create Vulkan instance: {}", exception.what());
             throw exception;
         }
+
+        // Initialize default dispatcher with our Vulkan instance to get access to
+        // instance specific functions
+        VULKAN_HPP_DEFAULT_DISPATCHER.init(*m_vulkanInstance);
+    }
+
+    std::vector<const char*> Instance::getRequiredExtensions()
+    {
+        // Query extensions required for rendering to SDL window
+        unsigned int sdlRequiredExtensionCount;
+        SDL_Vulkan_GetInstanceExtensions(nullptr, &sdlRequiredExtensionCount, nullptr);
+        const char** sdlRequiredExtensionNames = new const char*[sdlRequiredExtensionCount];
+        SDL_Vulkan_GetInstanceExtensions(nullptr, &sdlRequiredExtensionCount, sdlRequiredExtensionNames);
+
+        std::vector<const char*> requiredExtensions;
+
+        // Request the debug trace extension in debug builds
+        #ifndef NDEBUG
+        requiredExtensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        #endif
+
+        // Add the SDL extensions to the full list
+        for (unsigned int i = 0; i < sdlRequiredExtensionCount; i++)
+        {
+            requiredExtensions.push_back(sdlRequiredExtensionNames[i]);
+        }
+
+        return requiredExtensions;
     }
 
     std::vector<const char*> Instance::getSupportedValidationLayers()
     {
         #ifdef NDEBUG
-            return std::vector<const char*>();
+        return std::vector<const char*>();
         #else
-            static const std::vector<std::string> requestedLayers = {
-                "VK_LAYER_KHRONOS_validation"
-            };
+        // Request the Khronos validation in debug builds
+        static const std::vector<std::string> requestedLayers = {
+            "VK_LAYER_KHRONOS_validation"
+        };
 
-            std::vector<const char*> resultLayers;
+        std::vector<const char*> resultLayers;
 
-            auto supportedLayers = vk::enumerateInstanceLayerProperties();
-            for (auto& i : requestedLayers)
+        // Check to see if the validation layer is actually supported before requesting it
+        auto supportedLayers = vk::enumerateInstanceLayerProperties();
+        for (auto& i : requestedLayers)
+        {
+            for (auto& j : supportedLayers)
             {
-                for (auto& j : supportedLayers)
+                if (i == j.layerName)
                 {
-                    if (i == j.layerName)
-                    {
-                        // Vulkan creation info requires a C string, so we'll convert here
-                        resultLayers.push_back(i.c_str());
-                        break;
-                    }
+                    // Vulkan creation info requires a C string, so we'll convert here
+                    resultLayers.push_back(i.c_str());
+                    break;
                 }
             }
+        }
 
-            spdlog::info("{} of {} requested Vulkan validation layers supported", requestedLayers.size(), resultLayers.size());
-            return resultLayers;
+        spdlog::info("{} of {} requested Vulkan validation layers supported", requestedLayers.size(), resultLayers.size());
+        return resultLayers;
         #endif
     }
+
+    void Instance::setupVulkanDebug()
+    {
+        spdlog::info("Creating Vulkan debug messenger");
+
+        m_vulkanDebugMessenger = m_vulkanInstance->createDebugUtilsMessengerEXTUnique(
+            vk::DebugUtilsMessengerCreateInfoEXT{
+                {},
+                vk::DebugUtilsMessageSeverityFlagBitsEXT::eError |
+                    vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                    vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+                    vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo,
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+                vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance,
+            debugCallback}
+        );
+    }
 }
+
+
+
+// Disable warning about unreference formal parameters
+// 
+#pragma warning(push)
+#pragma warning(disable : 4100)
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData)
+{
+    switch (static_cast<vk::DebugUtilsMessageSeverityFlagBitsEXT>(messageSeverity))
+    {
+        case vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose:
+            spdlog::debug("Vulkan: {}", pCallbackData->pMessage);
+            break;
+        
+        case vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo:
+            spdlog::info("Vulkan: {}", pCallbackData->pMessage);
+            break;
+
+        case vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning:
+            spdlog::warn("Vulkan: {}", pCallbackData->pMessage);
+            break;
+
+        case vk::DebugUtilsMessageSeverityFlagBitsEXT::eError:
+            spdlog::error("Vulkan: {}", pCallbackData->pMessage);
+            break;
+    }
+
+    return VK_FALSE;
+}
+
+#pragma warning(pop)
+
--- src/rendering/instance.hpp
+++ src/rendering/instance.hpp
@@ -3,13 +3,7 @@
 // Defines a singleton to access a Vulkan instance
 // Also hooks in SDL initialization as it is required for Vulkan use
 
-#include <vector>
-
-// Forward declare vk::Instance
-namespace vk
-{
-    class Instance;
-}
+#include <vulkan/vulkan.hpp>
 
 namespace Rendering
 {
@@ -18,19 +12,24 @@ namespace Rendering
     class Instance
     {
         friend class Context;
-        
+
         protected:
             Instance();
             ~Instance();
 
-            vk::Instance& getVulkanInstance() const;
+            const vk::Instance& getVulkanInstance() const;
 
         private:
             // Initialization steps
             void initializeSdl();
             void initializeVulkan();
+            void setupVulkanDebug();
+
+            // Initialization helpers
+            std::vector<const char*> getRequiredExtensions();
             std::vector<const char*> getSupportedValidationLayers();
 
-            vk::Instance* m_vulkanInstance;
+            vk::UniqueInstance m_vulkanInstance;
+            vk::UniqueDebugUtilsMessengerEXT m_vulkanDebugMessenger;
     };
 }
